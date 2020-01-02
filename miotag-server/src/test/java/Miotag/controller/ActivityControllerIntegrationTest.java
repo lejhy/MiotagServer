fb2fa9ee83645418299f0498c6103e3d1fc0c513
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2020-01-02 18:46:26 +0100

    Activity integration tests

---
+++ miotag-server/src/test/java/Miotag/controller/ActivityControllerIntegrationTest.java
@@ -0,0 +1,189 @@
+package Miotag.controller;
+
+import Miotag.dto.ActivityDto;
+import Miotag.dto.ActivityLogDto;
+import Miotag.dto.UserDto;
+import Miotag.mapper.ActivityMapper;
+import Miotag.model.Activity;
+import Miotag.repository.ActivityRepository;
+import Miotag.service.IActivityService;
+import Miotag.service.ISecurityService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.*;
+
+import static Miotag.controller.Utils.generateUserDto;
+import static Miotag.controller.Utils.registerUser;
+import static org.junit.Assert.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@RunWith(SpringRunner.class)
+public class ActivityControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ISecurityService securityService;
+
+    @Autowired
+    IActivityService activityService;
+
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @Autowired
+    ActivityMapper activityMapper;
+
+    private Random random = new Random();
+
+    private Activity activity;
+
+    @Before
+    public  void setUp() throws Exception {
+        activity = activityRepository.save(generateActivity());
+    }
+
+    @Test
+    public void getActivities() throws Exception {
+        UserDto userUnderTest = generateUserDto();
+        userUnderTest.setId(registerUser(mockMvc, objectMapper, userUnderTest).getId());
+        List<Activity> activities = new ArrayList<>();
+        for(int i = 0; i < 8; i++) {
+            activities.add(activityRepository.save(generateActivity()));
+        }
+
+        MvcResult response = mockMvc.perform(get("/activities")
+                .with(httpBasic(userUnderTest.getEmail(), userUnderTest.getPassword()))
+        ).andExpect(status().isOk()).andReturn();
+        List<ActivityDto> activitiesReceived = objectMapper.readValue(response.getResponse().getContentAsString(), new TypeReference<List<ActivityDto>>(){});
+        activitiesReceived.sort(Comparator.comparingLong(ActivityDto::getId));
+        int activitiesToAssertStartIndex = 0;
+        for(int i = 0; i < activitiesReceived.size(); i++) {
+            if (activitiesReceived.get(i).getId() == activities.get(0).getId()) {
+                activitiesToAssertStartIndex = i;
+                break;
+            }
+        }
+        List<ActivityDto> activitiesToAssert = activitiesReceived.subList(activitiesToAssertStartIndex, activitiesReceived.size());
+
+        assertEquals(activities.size(), activitiesToAssert.size());
+        for(int i = 0; i < activities.size(); i++) {
+            Activity expected = activities.get(i);
+            ActivityDto received = activitiesToAssert.get(i);
+            assertEquals(expected.getId(), received.getId());
+            assertEquals(expected.getName(), received.getName());
+            assertEquals(expected.getDescription(), received.getDescription());
+        }
+    }
+
+    @Test
+    public void newAndGetActivityLog() throws Exception {
+        UserDto userUnderTest = generateUserDto();
+        userUnderTest.setId(registerUser(mockMvc, objectMapper, userUnderTest).getId());
+
+        ActivityDto activityDto = activityMapper.map(activity);
+        ActivityLogDto activityLogToSend = generateActivityLogDto(activityDto);
+        Date timestampBefore = new Date();
+        ActivityLogDto activityLogSent = postActivityLog(userUnderTest, activityLogToSend);
+        Date timestampAfter = new Date();
+        List<ActivityLogDto> activityLogsReceived = getActivityLog(userUnderTest, activityDto);
+
+        assertEquals(1, activityLogsReceived.size());
+        ActivityLogDto activityLogReceived = activityLogsReceived.get(0);
+
+        assertEquals(activityLogSent.getId(), activityLogReceived.getId());
+        assertEquals(activityLogToSend.getActivity().getId(), activityLogSent.getActivity().getId(), activityLogReceived.getActivity().getId());
+        assertTrue(timestampBefore.compareTo(activityLogSent.getDate()) <= 0);
+        assertTrue(timestampAfter.compareTo(activityLogSent.getDate()) >= 0);
+        assertEquals(activityLogSent.getDate(), activityLogReceived.getDate());
+        assertEquals(activityLogToSend.getScore(), activityLogSent.getScore(), activityLogReceived.getScore());
+        assertEquals(activityLogToSend.getLength(), activityLogSent.getLength(), activityLogReceived.getLength());
+    }
+
+    @Test
+    public void newAndGetActivityLogMultiple() throws Exception {
+        UserDto userUnderTest = generateUserDto();
+        userUnderTest.setId(registerUser(mockMvc, objectMapper, userUnderTest).getId());
+        ActivityDto activityDto = activityMapper.map(activity);
+
+        List<ActivityLogDto> activityLogsSent = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++) {
+            activityLogsSent.add(postActivityLog(userUnderTest, generateActivityLogDto(activityDto)));
+        }
+
+        List<ActivityLogDto> activityLogsReceived = getActivityLog(userUnderTest, activityDto);
+        activityLogsReceived.sort(Comparator.comparingLong(ActivityLogDto::getId));
+        for(int i = 0; i < activityLogsSent.size(); i++) {
+            ActivityLogDto expectedActivityLog = activityLogsSent.get(i);
+            ActivityLogDto receivedActivityLog = activityLogsReceived.get(i);
+
+            assertEquals(expectedActivityLog.getId(), receivedActivityLog.getId());
+            assertEquals(expectedActivityLog.getActivity().getId(), receivedActivityLog.getActivity().getId());
+            assertEquals(expectedActivityLog.getDate(), receivedActivityLog.getDate());
+            assertEquals(expectedActivityLog.getScore(), receivedActivityLog.getScore());
+            assertEquals(expectedActivityLog.getLength(), receivedActivityLog.getLength());
+        }
+    }
+
+    @Test
+    public void getActivityLogEmpty() {
+
+    }
+
+    private ActivityLogDto postActivityLog(UserDto userDto, ActivityLogDto activityLogDto) throws Exception {
+        MvcResult response = mockMvc.perform(post("/activities/logs")
+                .with(httpBasic(userDto.getEmail(), userDto.getPassword()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(activityLogDto))
+        ).andExpect(status().isOk()).andReturn();
+        return objectMapper.readValue(response.getResponse().getContentAsString(), ActivityLogDto.class);
+    }
+
+    private List<ActivityLogDto> getActivityLog(UserDto userDto, ActivityDto activityDto) throws Exception {
+        MvcResult response = mockMvc.perform(get("/activities/logs" + (activityDto == null ? "" : "/"+activityDto.getId()))
+                .with(httpBasic(userDto.getEmail(), userDto.getPassword()))
+        ).andExpect(status().isOk()).andReturn();
+        return objectMapper.readValue(response.getResponse().getContentAsString(), new TypeReference<List<ActivityLogDto>>(){});
+    }
+
+    private ActivityLogDto generateActivityLogDto(ActivityDto activityDto) {
+        ActivityLogDto activityLogDto = new ActivityLogDto();
+        activityLogDto.setActivity(activityDto);
+        activityLogDto.setLength(random.nextInt());
+        activityLogDto.setScore(random.nextInt());
+        return activityLogDto;
+    }
+
+    private Activity generateActivity() {
+        Activity activity = new Activity();
+        activity.setName("activityName"+ random.nextInt());
+        activity.setDescription("activityDescription"+ random.nextInt());
+        activity.setEnabled(true);
+        return activity;
+    }
+}
